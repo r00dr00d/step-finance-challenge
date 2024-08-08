b2a0539442cb6731b6108669aea198c9b4bd0112
Rodrigo Bloise <rgbmdq+step@gmail.com>
2024-08-08 20:28:18 -0300

    fix(StepStakingExercise): Validate amount

--- web/components/step-staking/StepStakingComponent.tsx
+++ web/components/step-staking/StepStakingComponent.tsx
@@ -7,7 +7,7 @@ import {
   useStepUnstakeOperation,
 } from './step-staking-data-access';
 import { TabHeadingButton } from '../ui/tabs';
-import { UI_AMOUNT_TO_AMOUNT_QTY } from '@/utils';
+import { UI_AMOUNT_TO_AMOUNT_QTY, validateStakeAmount, validateUnstakeAmount } from '@/utils';
 import { StakeComponent } from './StakeComponent';
 import { StakeIcon, UnstakeIcon } from '../ui/icons';
 import { UnstakeComponent } from './UnstakeComponent';
@@ -16,11 +16,16 @@ import { UnstakeComponent } from './UnstakeComponent';
 
 
 export function StepStakingComponent() {
-  const price = useStepPrice();
+  const priceReq = useStepPrice();
+  const price = priceReq?.data ?? 0;
   const balance = useStepStakingBalance();
 
   const [operation, setOperation] = useState<'stake' | 'unstake'>('stake');
   const [input, setInput] = useState<{ type: 'step' | 'xstep', qty: number }>({ type: 'step', qty: 0 });
+
+  const [error, errorLabel] = operation === 'stake'
+    ? validateStakeAmount(input, price, balance?.data?.step?.uiAmount)
+    : validateUnstakeAmount(input, price, balance?.data?.step?.uiAmount);
 
   const { mutateAsync: stake } = useStepStakeOperation();
   const { mutateAsync: unstake } = useStepUnstakeOperation();
@@ -33,16 +38,16 @@ export function StepStakingComponent() {
           <TabHeadingButton active={operation === 'unstake'} text="Unstake" icon={<UnstakeIcon />} onClick={() => setOperation('unstake')} />
         </div>
         <div className='flex flex-col gap-2 bg-[#141414] p-4 rounded-b-md rounded-tr-md w-[450px]'>
-          {operation === 'stake' ? <StakeComponent price={price?.data ?? 0} stepUiBalance={balance?.data?.step?.uiAmount} xStepUiBalance={balance?.data?.xStep?.uiAmount} input={input} onInputUpdate={setInput} /> : <UnstakeComponent price={price?.data ?? 0} stepUiBalance={balance?.data?.step?.uiAmount} xStepUiBalance={balance?.data?.xStep?.uiAmount} input={input} onInputUpdate={setInput} />}
+          {operation === 'stake' ? <StakeComponent price={price} stepUiBalance={balance?.data?.step?.uiAmount} xStepUiBalance={balance?.data?.xStep?.uiAmount} input={input} onInputUpdate={setInput} /> : <UnstakeComponent price={price} stepUiBalance={balance?.data?.step?.uiAmount} xStepUiBalance={balance?.data?.xStep?.uiAmount} input={input} onInputUpdate={setInput} />}
         </div>
       </div>
 
       <button
-        disabled={input.qty <= 0}
+        disabled={error}
         className="w-[450px] h-[60px] font-extrabold transition-colors duration-300 rounded-md text-[#06d6a0] bg-[#003628] hover:text-black hover:bg-[#06d6a0] disabled:bg-[#434343] disabled:text-[#b2b2b2] disabled:cursor-not-allowed"
         onClick={() => operation === 'stake' ? stake(input.qty * UI_AMOUNT_TO_AMOUNT_QTY) : unstake(input.qty * UI_AMOUNT_TO_AMOUNT_QTY)}
       >
-        {input.qty > 0 ? operation : 'Enter an amount'}
+        {error ? errorLabel : operation}
       </button>
     </div>
   )
--- web/utils/index.ts
+++ web/utils/index.ts
@@ -8,3 +8,30 @@ export const stepToXStep = (step: number, price: number) => {
 export const xStepToStep = (xStep: number, price: number) => {
   return xStep * price;
 }
+
+export const validateStakeAmount = (input: { type: 'step' | 'xstep', qty: number }, price: number, amount: number) : [boolean, string] => {
+  const step = input.type === 'step' ? input.qty : xStepToStep(input.qty, price);
+
+
+  if (step === 0)
+    return [true, `Enter an amount`]
+
+  if (step > amount)
+    return [true, `Insufficient STEP balance`]
+
+
+  return [false, ''];
+}
+
+export const validateUnstakeAmount = (input: { type: 'step' | 'xstep', qty: number }, price: number, amount: number) : [boolean, string] => {
+  const xStep = input.type === 'xstep' ? input.qty : stepToXStep(input.qty, price);
+  
+  if (xStep === 0)
+    return [true, `Enter an amount`]
+
+  if (xStep > amount)
+    return [true, `Insufficient xSTEP balance`]
+
+
+  return [false, ''];
+}
