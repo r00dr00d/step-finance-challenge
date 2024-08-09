2aea7d730aae7b0768ed14a3e715840f5b4567d4
Rodrigo Bloise <rgbmdq+step@gmail.com>
2024-08-08 21:38:45 -0300

    feat: visual logic

--- web/components/step-staking/StepStakingBox.tsx
+++ web/components/step-staking/StepStakingBox.tsx
@@ -19,9 +19,76 @@ const buttonStyles = 'w-[450px] h-[60px] font-extrabold transition-colors durati
 const activeButtonStyles = 'text-[#06d6a0] bg-[#003628] hover:text-black hover:bg-[#06d6a0]';
 const inactiveButtonStyles = 'bg-[#434343] text-[#b2b2b2] cursor-not-allowed';
 
-export function StepStakingBox() {
-  const [activeTab, setActiveTab] = useState<'stake' | 'unstake'>('stake')
-  const amount=true
+interface StepStakingBoxProps {
+  stepBalance: string;
+  xStepBalance: string;
+  stepToXstep: number | null;
+  xStepToStep: number | null;
+}
+
+export const StepStakingBox: React.FC<StepStakingBoxProps> = ({ stepToXstep, xStepToStep, stepBalance, xStepBalance }) => {
+  const [activeTab, setActiveTab] = useState<'stake' | 'unstake'>('unstake')
+  const [stake, setStake] = useState<number | null>()
+  const [recive, setRecive] = useState<number | null>()
+
+
+
+  const handleStake = (e: any) => {
+    setStake(e.target.value);
+    if(stepToXstep){
+      if(e.target.value){
+        setRecive(e.target.value * stepToXstep)
+      }else {
+        setRecive(null)
+      }
+    }
+  };
+
+  const handleMax = () => {
+    if(activeTab === 'stake'){
+      setStake(Number(stepBalance));
+      if(stepToXstep){
+        if(Number(stepBalance)){
+          setRecive(Number(stepBalance) * stepToXstep)
+        }else {
+          setRecive(null)
+        }
+      }
+    } else {
+      setStake(Number(xStepBalance));
+      if(xStepToStep){
+        if(Number(xStepBalance)){
+          setRecive(Number(xStepBalance) * xStepToStep)
+        }else {
+          setRecive(null)
+        }
+      }
+    }
+  };
+
+  const handleHalf = () => {
+    if(activeTab === 'stake'){
+      setStake(Number(stepBalance) / 2);
+      if(stepToXstep){
+        if(Number(stepBalance)){
+          setRecive((Number(stepBalance) / 2) * stepToXstep)
+        }else {
+          setRecive(null)
+        }
+      }
+    } else {
+      setStake(Number(xStepBalance)/2);
+      if(xStepToStep){
+        if(Number(xStepBalance)){
+          setRecive((Number(xStepBalance) / 2) * xStepToStep)
+        }else {
+          setRecive(null)
+        }
+      }
+    }
+  };
+
+
   return (
     <div className='flex flex-col gap-3'>
       <div>
@@ -33,9 +100,9 @@ export function StepStakingBox() {
           <div className="flex justify-between items-center text-sm">
             <span className="text-[white]">You stake</span>
             <div className='flex gap-2 items-center'>
-              <span className="text-[#7d7d7d] font-mono">Balance: 132.00000</span>
-              <button className='text-xs p-1 transition-color duration-300 hover:text-black hover:bg-[#06d6a0] text-[#06d6a0] bg-[#003628] rounded-md uppercase'>Half</button>
-              <button className='text-xs p-1 transition-color duration-300 hover:text-black hover:bg-[#06d6a0] text-[#06d6a0] bg-[#003628] rounded-md uppercase'>Max</button>
+              <span className="text-[#7d7d7d] font-mono">Balance:{ (activeTab === 'stake' ? stepBalance: xStepBalance ) || ""}</span>
+              <button className='text-xs p-1 transition-color duration-300 hover:text-black hover:bg-[#06d6a0] text-[#06d6a0] bg-[#003628] rounded-md uppercase' onClick={handleHalf}>Half</button>
+              <button className='text-xs p-1 transition-color duration-300 hover:text-black hover:bg-[#06d6a0] text-[#06d6a0] bg-[#003628] rounded-md uppercase' onClick={handleMax}>Max</button>
             </div>
           </div>
           <div className='bg-black flex justify-between px-2 py-4 rounded-md'>
@@ -44,8 +111,8 @@ export function StepStakingBox() {
               <span>STEP</span>
             </div>
             <div className='flex flex-col items-end'>
-              <input type="text" value="66.0000000" />
-              <span>$3.83</span>
+              <input placeholder='0.00' className='placeholder-gray-400 font-mono text-base text-[white] text-right appearance: none; border-none bg-transparent outline-none ' type="text" value={stake} onChange={handleStake} />
+              {stake && stake > 0 ? <span className='text-xs text-[#7d7d7d]'>$3.83</span> : null}
             </div>
           </div>
           <div className='flex py-4 items-center justify-center'>
@@ -54,7 +121,7 @@ export function StepStakingBox() {
           <div className="flex justify-between items-center text-sm">
             <span className="text-[white]">You receive</span>
             <div className='flex gap-2 items-center'>
-              <span className="text-[#7d7d7d] font-mono">Balance: 3.00000</span>
+              <span className="text-[#7d7d7d] font-mono">Balance:{ (activeTab === 'stake' ? xStepBalance: stepBalance ) || ""}</span>
             </div>
           </div>
           <div className='bg-black flex justify-between px-2 py-4 rounded-md'>
@@ -63,15 +130,22 @@ export function StepStakingBox() {
               <span>xSTEP</span>
             </div>
             <div className='flex flex-col items-end'>
-              <input type="text" value="66.0000000" />
-              <span>$3.83</span>
+              <input placeholder='0.00' className='placeholder-gray-40 font-mono text-base text-[white] text-right appearance: none; border-none bg-transparent outline-none' type="text" value={recive ? recive : ""} />
+              {stake && stake > 0 ? <span className='text-xs text-[#7d7d7d]'>$3.83</span> : null}
             </div>
           </div>
         </div>
       </div>
 
-      <button className={`${buttonStyles} ${amount ? activeButtonStyles : inactiveButtonStyles}`}>
-        {amount ? 'Stake' : 'Enter an amount'}
+      <button className={`${buttonStyles} ${stake && stake > 0 ? activeButtonStyles : inactiveButtonStyles}`}>
+        {activeTab === 'stake'
+          ? stake && stake > 0
+            ? 'Stake'
+            : 'Enter an amount'
+          : stake && stake > 0
+          ? 'Unstake'
+          : 'Enter an amount'
+        }
       </button>
     </div>
   )
--- web/components/step-staking/StepStakingComponent.tsx
+++ web/components/step-staking/StepStakingComponent.tsx
@@ -8,21 +8,24 @@ export function StepStakingComponent() {
   const { wallet } = useAnchorProvider()
   const balance = useStepStakingBalance()
   const { mutateAsync: stake } = useStepStakeOperation()
-  const { mutateAsync: unstake } = useStepUnstakeOperation()
+  const { mutateAsync: unstake } = useStepUnstakeOperation()  
+  const stepToXstep = price?.data ? 1 * (1 / price.data) : null; 
+  const xStepToStep =  price?.data ? 1 * price?.data : null; 
 
-  return <StepStakingBox />
-
-  // return <div>
-  //   <p className="py-4">Address: {wallet.publicKey.toString()}</p>
-  //   <p className="py-4">Step Balance: {balance.data?.step?.uiAmount}</p>
-  //   <p className="py-4">xStep Balance: {balance.data?.xStep?.uiAmount}</p>
-  //   {price?.data ? <>
-  //     <p className="py-4">1 step equals to {1 * (1 / price?.data)} xStep</p>
-  //     <p className="py-4">1 xStep equals to {1 * price?.data} Step</p>
-  //   </> : null}
-  //   <div className="flex items-center justify-center gap-3">
-  //     <button className="bg-white text-black p-2 rounded" onClick={() => stake(2000000000).then(console.log).catch(console.error)}>Stake!</button>
-  //     <button className="bg-white text-black p-2 rounded" onClick={() => unstake(2000000000).then(console.log).catch(console.error)}>Unstake!</button>
-  //   </div>
-  // </div>
+  return <div>
+    <StepStakingBox stepToXstep={stepToXstep} xStepToStep={xStepToStep} stepBalance={balance.data?.step?.uiAmount} xStepBalance={balance.data?.xStep?.uiAmount} />
+    <div>
+      <p className="py-4">Address: {wallet.publicKey.toString()}</p>
+      <p className="py-4">Step Balance: {balance.data?.step?.uiAmount}</p>
+      <p className="py-4">xStep Balance: {balance.data?.xStep?.uiAmount}</p>
+      {price?.data ? <>
+        <p className="py-4">1 step equals to {1 * (1 / price?.data)} xStep</p>
+        <p className="py-4">1 xStep equals to {1 * price?.data} Step</p>
+      </> : null}
+      <div className="flex items-center justify-center gap-3">
+        <button className="bg-white text-black p-2 rounded" onClick={() => stake(2000000000).then(console.log).catch(console.error)}>Stake!</button>
+        <button className="bg-white text-black p-2 rounded" onClick={() => unstake(2000000000).then(console.log).catch(console.error)}>Unstake!</button>
+      </div>
+    </div>
+  </div>
 }
