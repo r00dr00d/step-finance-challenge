975624c79674c32197567fad50e51dd05d5daf58
Rodrigo Bloise <rgbmdq+step@gmail.com>
2024-08-08 21:39:03 -0300

    feat: visual logic

--- web/components/step-staking/StepStakingComponent.tsx
+++ web/components/step-staking/StepStakingComponent.tsx
@@ -1,31 +1,26 @@
 'use client';
-import { useAnchorProvider } from "../solana/solana-provider";
-import { StepStakingBox } from "./StepStakingBox";
-import { useStepPrice, useStepStakeOperation, useStepStakingBalance, useStepUnstakeOperation } from "./step-staking-data-access";
+import { StepStakingBox } from './StepStakingBox';
+import {
+  useStepPrice,
+  useStepStakeOperation,
+  useStepStakingBalance,
+  useStepUnstakeOperation,
+} from './step-staking-data-access';
 
 export function StepStakingComponent() {
-  const price = useStepPrice()
-  const { wallet } = useAnchorProvider()
-  const balance = useStepStakingBalance()
-  const { mutateAsync: stake } = useStepStakeOperation()
-  const { mutateAsync: unstake } = useStepUnstakeOperation()  
-  const stepToXstep = price?.data ? 1 * (1 / price.data) : null; 
-  const xStepToStep =  price?.data ? 1 * price?.data : null; 
+  const price = useStepPrice();
+  const balance = useStepStakingBalance();
+  const { mutateAsync: stake } = useStepStakeOperation();
+  const { mutateAsync: unstake } = useStepUnstakeOperation();
+  const stepToXstep = price?.data ? 1 * (1 / price.data) : null;
+  const xStepToStep = price?.data ? 1 * price?.data : null;
 
-  return <div>
-    <StepStakingBox stepToXstep={stepToXstep} xStepToStep={xStepToStep} stepBalance={balance.data?.step?.uiAmount} xStepBalance={balance.data?.xStep?.uiAmount} />
-    <div>
-      <p className="py-4">Address: {wallet.publicKey.toString()}</p>
-      <p className="py-4">Step Balance: {balance.data?.step?.uiAmount}</p>
-      <p className="py-4">xStep Balance: {balance.data?.xStep?.uiAmount}</p>
-      {price?.data ? <>
-        <p className="py-4">1 step equals to {1 * (1 / price?.data)} xStep</p>
-        <p className="py-4">1 xStep equals to {1 * price?.data} Step</p>
-      </> : null}
-      <div className="flex items-center justify-center gap-3">
-        <button className="bg-white text-black p-2 rounded" onClick={() => stake(2000000000).then(console.log).catch(console.error)}>Stake!</button>
-        <button className="bg-white text-black p-2 rounded" onClick={() => unstake(2000000000).then(console.log).catch(console.error)}>Unstake!</button>
-      </div>
-    </div>
-  </div>
+  return (
+    <StepStakingBox
+      stepToXstep={stepToXstep}
+      xStepToStep={xStepToStep}
+      stepBalance={balance.data?.step?.uiAmount}
+      xStepBalance={balance.data?.xStep?.uiAmount}
+    />
+  );
 }
